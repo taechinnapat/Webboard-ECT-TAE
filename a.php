e2fec2449bb8836ba0123d34f3e2a76993ae96b4
taechinnapat <techinnapat@gmail.com>
2024-01-03 13:43:52 +0700

    Example 1 in php

---
+++ a.php
@@ -0,0 +1,10 @@
+<?php 
+    $a = 4;
+    $b = 10;
+
+    echo "Result 1 = " . ($a + $b) . "<BR>";
+    echo "Result 2 = " . ($b - $a) . "<BR>";
+    echo "Result 3 = " . ($a * $b) . "<BR>";
+    echo "Result 4 = " . ($b / $a) . "<BR>";
+
+?>
