72d1bffea97f6b28ce79f69b5d44a2c0d4f93999
taechinnapat <techinnapat@gmail.com>
2024-01-03 15:47:48 +0700

    Work4 php

---
+++ post.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard TAE</h1>
+    <hr>
+    <div style="text-align: center;"> ต้องการแสดงกระทู้หมายเลข 
+    <?php echo "$_GET[id]" ?>
+    </div>
+    <table style="border: 2px solid black; width: 40%;" align="center">
+        <tr><td colspan="2" style="background-color: #6CD2FE;">แสดงความคิดเห็น</td></tr>
+        <tr><td><textarea name="message"row="10" cols="70"></textarea> </td></tr>
+        <tr><td style="text-align: center;"><input type="submit" value="ส่งข้อความ"></td></tr>
+    </table>
+    <div style="text-align: center;"><a href="index.html">กลับไปหน้าหลัก</a></div>
+</body>
+</html>
