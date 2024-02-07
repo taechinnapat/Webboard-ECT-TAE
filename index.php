ade8dbf87174a33a6de98468b9929e4f2c39f887
taechinnapat <techinnapat@gmail.com>
2024-02-07 16:51:53 +0700

    index.php: navbar

--- index.php
+++ index.php
@@ -4,13 +4,39 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Webboard TAE</title>
+    <title>Webboard KakKak</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard TAE</h1>
-    <hr>
+    <div class="container-lg">
+    <h1 style="text-align: center;" class="m-3">Webboard KakKak</h1>
+    <nav class="navbar navbar-expand-lg"style="background-color: #d3d3d3;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
+      <ul class="navbar-nav">
+        <?php if(!isset($_SESSION["id"])){ ?>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+        </li>
+        <?php }else{ ?>
+        <li class="nav-item dropdown">
+        <a class="btn btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-person-lines-fill"></i> <?php echo $_SESSION['username'] ?>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+            
+          </ul>
+        </li>
+        <?php } ?>
+
+      </ul>
+  </div>
+</nav>
     <form>
         หมวดหมู่:
         <select>
@@ -18,6 +44,8 @@ session_start();
             <option value="general">เรื่องทั่วไป</option>
             <option value="study">เรื่องเรียน</option>
         </select>
+
+        </nav>
         <?php
         if(!isset($_SESSION["id"])){
             echo "<a href= login.php style='float: right;'>เข้าสู่ระบบ</a>";
@@ -44,5 +72,6 @@ session_start();
         ?>
 
     </ul>
+    </div>
 </body>
 </html>
